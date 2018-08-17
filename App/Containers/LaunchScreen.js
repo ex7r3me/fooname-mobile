981dc02b9921f1b24deab790935cf0b55e1e84ec
ex7r3me <siavash.ahmadpour@gmail.com>
2018-08-17 23:59:50 +0430

    Add Profile screen
    - add profile screen after login with twitter

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,44 +1,38 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View, Button } from 'react-native'
-import { Images } from '../Themes'
-import Auth0 from 'react-native-auth0';
+import Auth0 from 'react-native-auth0'
 const auth0 = new Auth0({
   domain: 'fooname.eu.auth0.com',
   clientId: '7mgRrH24na7Rt3X-75AsZRuRZyJ7DuHl'
 })
-
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
-  loginWithTwitter () {
+  openProfileScreen = (credentials) => {
+    this.props.navigation.navigate('ProfileScreen', {credentials})
+  }
+  loginWithTwitter = () => {
     auth0
       .webAuth
       .authorize({ scope: 'openid profile email', audience: 'https://fooname.eu.auth0.com/userinfo' })
-      .then(credentials =>
-        console.log(credentials)
-        // Successfully authenticated
-        // Store the accessToken
+      .then(credentials => {
+        this.openProfileScreen(credentials)
+      }
       )
       .catch(error => console.log(error))
   }
   render () {
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
           <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
+            <View style={styles.centered}>
+              <Text>Welcome to fooName</Text>
+              <Text>Login with Twitter to continue</Text>
+            </View>
             <Button onPress={this.loginWithTwitter} title='Login with twitter' />
           </View>
-
         </ScrollView>
       </View>
     )
---
+++ App/Containers/ProfileScreen.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
+import { connect } from 'react-redux'
+// Add Actions - replace 'Your' with whatever your reducer is called :)
+// import YourActions from '../Redux/YourRedux'
+
+// Styles
+import styles from './Styles/ProfileScreenStyle'
+
+class ProfileScreen extends Component {
+  render () {
+    const { navigation } = this.props
+    const credentials = JSON.stringify(navigation.getParam('credentials', 'Not logged in'))
+    return (
+      <ScrollView style={styles.container}>
+        <KeyboardAvoidingView behavior='position'>
+          <Text>User Profile</Text>
+          <Text>{credentials}</Text>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileScreen)

ce01648c74e740de48494652c844f1510ce7c702
ex7r3me <siavash.ahmadpour@gmail.com>
2018-08-27 14:04:43 +0430

    Add logout in drawer
    - add a logout button in drawer ( custom component)

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,13 +1,21 @@
 import {
   createStackNavigator,
   createSwitchNavigator,
-  createDrawerNavigator
+  createDrawerNavigator,
+  DrawerItems
 } from 'react-navigation'
 import AuthLoadingScreen from '../Containers/AuthLoadingScreen'
 import ProfileScreen from '../Containers/ProfileScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
-
+import { View, SafeAreaView, Button, AsyncStorage } from 'react-native'
+import React from 'react'
 import styles from './Styles/NavigationStyles'
+import API from '../../App/Services/Api'
+
+const _logout = async () => {
+  // todo Add API Call
+  await AsyncStorage.removeItem('accessToken')
+}
 
 const AppDrawer = createDrawerNavigator(
   {
@@ -18,6 +26,20 @@ const AppDrawer = createDrawerNavigator(
   },
   {
     // Default config for all screens
+    contentComponent: props => (
+      <View style={{ flex: 1 }}>
+        <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+          <DrawerItems {...props} />
+          <Button
+            title='Logout'
+            onPress={() => {
+              _logout()
+              props.navigation.navigate('Auth')
+            }}
+          />
+        </SafeAreaView>
+      </View>
+    ),
     headerMode: 'none',
     initialRouteName: 'ProfileScreen',
     navigationOptions: {

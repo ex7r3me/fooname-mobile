6885e5842171e0bb1176a5d58b271b0427ba9cd1
ex7r3me <siavash.ahmadpour@gmail.com>
2018-09-13 14:16:07 +0430

    Remove launch screen additional buttons
    - removed 2 buttons as we are getting closer to release

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -26,26 +26,6 @@ export default class LaunchScreen extends Component {
           }}
           text='Login with Twitter'
         />
-        <Button
-          raised
-          primary
-          style={styles.button}
-          onPress={() => {
-            this.props.navigation.navigate('ProfileScreen')
-          }}
-          text='Profile'
-        />
-        <Button
-          raised
-          primary
-          style={styles.button}
-          onPress={() => {
-            this.props.navigation.navigate('ProfileScreen', {
-              credentials: '4EG6X4CjAijdPSTKWAiK4lHCD54nLHsxS0sDYnl9jsDDxkFcN8ZG7pEYqiuW9J5I'
-            })
-          }}
-          text='Profile set'
-        />
       </View>
     )
   }
